d24467a682bf6339e9884e975240492c154d3773
ayushpy0 <168622573+ayushpy0@users.noreply.github.com>
2026-06-01 11:23:03 +0530

    Week 1 Day 1 twoSum

---
+++ Week_1/Day_1/twoSum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    int i,j=0,n=nums.size();
+    for(i=1;i<n;i++){
+        if(nums[j]!=nums[i]){
+            nums[++j]=nums[i];
+        }
+    }
+    return j+1;
+}
+
+int main(){
+    vector<int> v = {1,1,2};
+    cout << removeDuplicates(v) << endl;
+    return 0;
+}
