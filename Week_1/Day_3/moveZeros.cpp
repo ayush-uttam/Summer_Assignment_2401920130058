b434e03384092fd98f90f940eab3d1682fe9d93c
ayushpy0 <168622573+ayushpy0@users.noreply.github.com>
2026-06-03 09:45:11 +0530

    Move Zeros

---
+++ Week_1/Day_3/moveZeros.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+    int i,ind=-1,n=nums.size();
+    for(i=0;i<n;i++){
+        if(nums[i]==0){
+            ind=i;
+            break;
+        }
+    }
+    if(ind==-1) return;
+    for(i=ind;i<n;i++){
+        if(nums[i]!=0){
+            swap(nums[ind++],nums[i]);
+        }
+    }
+}
+
+int main(){
+    vector<int> v={0,1,0,3,12};
+    moveZeroes(v);
+    for(auto x:v){
+        cout << x << " " ;
+    }
+    return 0;
+}
