ebf9726067cbb493776a4dbdada6c339ea27d422
ayush-uttam <24115cn056@glbitm.ac.in>
2026-06-05 10:07:18 +0530

    Longest common prefix

---
+++ Week_1/Day_5/commonPrefix.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    int i,j,l=strs.size();
+    int sl,mi=INT_MAX;
+    string res="";
+    for(i=0;i<l;i++){
+        sl=strs[i].size();
+        mi=min(sl,mi);
+    }
+    string t=strs[0];
+    for(i=0;i<mi;i++){
+        bool b=true;
+        for(j=1;j<l;j++){
+            string t1=strs[j];
+            if(t[i]!=t1[i]){
+                b=false;
+                break;
+            }
+        }
+        if(b)
+            res+=t[i];
+        else
+            break;
+    }
+    return res;
+}
+
+int main(){
+    vector<string> s={"flower","flow","flight"};
+    cout << longestCommonPrefix(s) << endl;
+    return 0;
+}
