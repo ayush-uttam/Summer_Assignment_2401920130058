0cbb275921968b95968bf1e6fcd7efc73b608296
ayush-uttam <24115cn056@glbitm.ac.in>
2026-06-12 19:33:05 +0530

    Longest Palindromic Substring

---
+++ Week_2/Day_5/longestPalinSub.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string longestPalindrome(string s) {
+    int n=s.size(),start=0,maxlen=1;
+    for(int i=0;i<n;i++){
+        int l=i,r=i;
+        while(l>=0&&r<n&&s[l]==s[r]){
+            if(r-l+1>maxlen){
+                maxlen=r-l+1;
+                start=l;
+            }
+            l--;
+            r++;
+        }
+        l=i;
+        r=i+1;
+        while(l>=0&&r<n&&s[l]==s[r]){
+            if(r-l+1>maxlen){
+                maxlen=r-l+1;
+                start=l;
+            }
+            l--;
+            r++;
+        }
+    }
+    return s.substr(start,maxlen);
+}
+
+int main(){
+    cout<< longestPalindrome("babad")<<endl;
+    return 0;
+}
