e274282f3507933a388ebe0c5f8f86a28eadab81
ayush-uttam <24115cn056@glbitm.ac.in>
2026-06-19 16:57:29 +0530

    Recent calls

---
+++ Week_3/Day_5/recentCalls.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class RecentCounter {
+public:
+    queue<int> q;
+    RecentCounter() {
+    }
+    int ping(int t) {
+        q.push(t);
+        while(!q.empty() && q.front() < t - 3000)
+            q.pop();
+        return q.size();
+    }
+};
+
+int main() {
+    RecentCounter ob;
+    cout<<ob.ping(1)<<endl;   
+    cout<<ob.ping(100)<<endl; 
+    cout<<ob.ping(3001)<<endl;
+    cout<<ob.ping(3002)<<endl;
+    return 0;
+}
